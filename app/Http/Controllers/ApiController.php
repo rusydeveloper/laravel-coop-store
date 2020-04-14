44fcc9741831b5a6f02124cc89fb9638bcad7119
rusydeveloper <rusy.developer@gmail.com>
2020-04-15 05:04:15 +0700

    updated

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -94,7 +94,7 @@ class ApiController extends Controller
 
     public function category()
     {
-        $categories = Category::orderBy("name")->get();
+        $categories = Category::where('parent', '0')->orderBy("name")->get();
 
         return response()->json($categories);
     }
