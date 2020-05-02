13c130100d5274689c1b3d017cd9d136b70dd492
rusydeveloper <rusy.developer@gmail.com>
2020-05-02 17:33:38 +0700

    updates

--- resources/views/admins/users/edit.blade.php
+++ resources/views/admins/users/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <style type="text/css">
-    .card-body{
+    .card-body {
         font-size: 12pt;
         text-align: left
     }
@@ -16,16 +16,18 @@
                 <div class="card-body">
                     <form method="POST" action="{{route('admin_user_update')}}">
                         @csrf
-                            <input type="hidden" name="user_id" value="{{$item->unique_id}}">
+                        <input type="hidden" name="user_id" value="{{$item->unique_id}}">
 
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{$item->name}}" required autofocus>
+                                <input id="name" type="text"
+                                    class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"
+                                    value="{{$item->name}}" required autofocus>
                                 @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>
@@ -33,12 +35,14 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">email address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{$item->email}}" required>
+                                <input id="email" type="email"
+                                    class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                                    value="{{$item->email}}" required>
 
                                 @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>
@@ -48,42 +52,46 @@
                             <label for="phone" class="col-md-4 col-form-label text-md-right">Phone Number</label>
 
                             <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{$item->phone}}" required>
+                                <input id="phone" type="text"
+                                    class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone"
+                                    value="{{$item->phone}}" required>
 
                                 @if ($errors->has('phone'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('phone') }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('phone') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="role" class="col-md-4 col-form-label text-md-right">Role</label>
                             <div class="col-md-6">
-                                <select id="role" name="role" class="form-control{{ $errors->has('role') ? ' is-invalid' : '' }}">
+                                <select id="role" name="role"
+                                    class="form-control{{ $errors->has('role') ? ' is-invalid' : '' }}">
                                     <option value="{{$item->role}}">{{$item->role}}</option>
-                                    <option value="tenant">Tenant</option>
-                                    <option value="cashier">Cashier</option>
+                                    <option value="cooperative">Cooperative</option>
+                                    <option value="supplier">Supplier</option>
                                 </select>
                                 @if ($errors->has('role'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('role') }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('role') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="status" class="col-md-4 col-form-label text-md-right">Status</label>
                             <div class="col-md-6">
-                                <select id="status" name="status" class="form-control{{ $errors->has('status') ? ' is-invalid' : '' }}">
+                                <select id="status" name="status"
+                                    class="form-control{{ $errors->has('status') ? ' is-invalid' : '' }}">
                                     <option value="{{$item->status}}">{{$item->status}}</option>
                                     <option value="active">Active</option>
                                     <option value="non active">Non Active</option>
                                 </select>
                                 @if ($errors->has('role'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('role') }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('role') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>
